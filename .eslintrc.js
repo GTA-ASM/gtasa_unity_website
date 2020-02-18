a1da0c2f242024aa23e8671e5cf3265b9a09a9dd
D4RKAR117 <D4RKAR117@outlook.es>
2020-02-18 17:49:35 -0500

    Added I18N, And configured github pages deployment (Docs Folder Instead Dist)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
@@ -10,11 +12,21 @@ module.exports = {
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint"
   ],
+
   parserOptions: {
     ecmaVersion: 2020
   },
+
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  }
+    'no-console': 'off',
+    'no-debugger': 'off'
+  },
+
+  'extends': [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ]
 };
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "i18n",
+      enableInSFC: true
+    }
+  },
+
+  runtimeCompiler: true,
+  outputDir: "docs",
+  assetsDir: "assets"
+};
