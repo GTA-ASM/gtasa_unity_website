4652348e65343e01f6eaf3065e7959bfc70e54f8
D4RKAR117 <D4RKAR117@outlook.es>
2020-02-18 20:31:12 -0500

    Started base Migration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import i18n from './i18n';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+import i18n from '@/i18n';
+
+import '@/assets/scss/app.scss';
+import 'bootstrap';
+
+import BootstrapVue from 'bootstrap-vue';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
